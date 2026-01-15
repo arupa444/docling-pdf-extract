908af1b98216e22e1e0993b2428bf2fe3b82289d
arupanandaswain <arupaswain7735@gmail.com>
2026-01-16 01:25:58 +0530

    updated RAG_On_nonJS_nonSPA_Website endpoint in app.py

--- app.py
+++ app.py
@@ -409,14 +409,53 @@ async def RAG_On_Folder_Or_Multiple_file_Uploads(
 
 
 @app.post("/RAG_On_nonJS_nonSPA_Website", summary="RAG on Non js and Non SPA website")
-async def RAG_On_nonJS_nonSPA_Website(webLink: str = Form(...),
-        subDir: str = Form('')):
+async def RAG_On_nonJS_nonSPA_Website(
+        webLink: str = Form(...),
+        query: str = Form(...),
+        subDir: str = Form('')
+):
     try:
         markdown_content = await DataExtAndRenderingService.anyThingButJSOrSPA(webLink)
-        config.storeMDContent(markdown_content, subDir)
-        return {"markdown_content": markdown_content}
+        savedLocation = config.storeMDContent(markdown_content, subDir)
+        ac = agenticChunker.AgenticChunker()
+
+        # 1. Raw Text Input
+        raw_text = markdown_content
+
+        # 2. Ingest Data (Layer 1)
+        propositions = ac.generate_propositions(raw_text)
+
+        print(f"\n[bold cyan]Generated {len(propositions)} Propositions[/bold cyan]")
+
+        ac.add_propositions(propositions)
+        ac.pretty_print_chunks()
+
+        # 3. Build Memory Index (Layer 3)
+        #    We initialize this AFTER ingestion is done.
+        print("\n[bold blue]Building Memory Index...[/bold blue]")
+        memory_index = chunkMemoryIndex.ChunkMemoryIndex(dim=768)
+
+        for chunk_id, chunk_data in ac.chunks.items():
+            memory_index.add(chunk_id, chunk_data['embedding'])
+
+        # 4. Retrieval (Layer 4)
+        retrieved_docs = DBretrieve.Retrieve.retrieve(query, ac, memory_index)
+
+        print(f"\n[green]Top Result:[/green] {retrieved_docs[0]['title']} (Score: {retrieved_docs[0]['score']:.4f})")
+
+        # 5. RAG Answer (Layer 5)
+        print("\n[bold blue]Generating Answer...[/bold blue]")
+
+        final_answer = ragAnswer.Answer.answer(query, retrieved_docs, ac.llm)
+        print(f"\n[bold]Final Answer:[/bold]\n{final_answer}")
+
+        config.save_results(savedLocation, propositions, ac.chunks, memory_index, subDir)
+        return {"Top Result": f"{retrieved_docs[0]['title']} (Score: {retrieved_docs[0]['score']:.4f})",
+                "Final Answer": final_answer, "markdown_content": markdown_content, "SavedLocation": savedLocation}
+
     except Exception as e:
         return {"error": str(e)}
+
 
 
 @app.post("/RAG_On_Multiple_nonJS_nonSPA_Website", summary="RAG on Multiple Non js and Non SPA website")
